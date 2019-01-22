8a94e54a2014d3427b28112e87051d837645d991
zorino <maximilien1er@gmail.com>
2019-01-22 13:54:05 -0500

    badger TableLoadingMode now MemoryMap to decrease RAM usage
    Signed-off-by: zorino <maximilien1er@gmail.com>

--- cmd/makedb/makedb.go
+++ cmd/makedb/makedb.go
@@ -57,26 +57,31 @@ func (kvStores *KVStores) Init (dbPath string) {
 	k_opts.Dir = dbPath+"/k_store"
 	k_opts.ValueDir = dbPath+"/k_store"
 	k_opts.ValueLogLoadingMode = options.FileIO
+	k_opts.TableLoadingMode = options.MemoryMap
 
 	g_opts := badger.DefaultOptions
 	g_opts.Dir = dbPath+"/g_store"
 	g_opts.ValueDir = dbPath+"/g_store"
 	g_opts.ValueLogLoadingMode = options.FileIO
+	g_opts.TableLoadingMode = options.MemoryMap
 
 	f_opts := badger.DefaultOptions
 	f_opts.Dir = dbPath+"/f_store"
 	f_opts.ValueDir = dbPath+"/f_store"
 	f_opts.ValueLogLoadingMode = options.FileIO
+	f_opts.TableLoadingMode = options.MemoryMap
 
 	p_opts := badger.DefaultOptions
 	p_opts.Dir = dbPath+"/p_store"
 	p_opts.ValueDir = dbPath+"/p_store"
 	p_opts.ValueLogLoadingMode = options.FileIO
+	p_opts.TableLoadingMode = options.MemoryMap
 
 	o_opts := badger.DefaultOptions
 	o_opts.Dir = dbPath+"/o_store"
 	o_opts.ValueDir = dbPath+"/o_store"
 	o_opts.ValueLogLoadingMode = options.FileIO
+	o_opts.TableLoadingMode = options.MemoryMap
 
 	kvStores.k_batch = kvstore.K_New(k_opts, 1000)
 	kvStores.g_batch = kvstore.G_New(g_opts, 1000)
