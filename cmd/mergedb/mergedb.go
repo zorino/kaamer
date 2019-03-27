4207a4f4d47904edcd9ca087076d05134db1852c
Maxime Deraspe <maximilien1er@gmail.com>
2019-03-27 10:28:06 -0400

    edit merge function for concurrent KV merge
    Signed-off-by: Maxime Deraspe <maximilien1er@gmail.com>

--- cmd/mergedb/mergedb.go
+++ cmd/mergedb/mergedb.go
@@ -1,29 +1,39 @@
 package mergedb
 
 import (
+	copy "github.com/zorino/metaprot/internal/helper"
 	"github.com/zorino/metaprot/pkg/kvstore"
-	// "github.com/dgraph-io/badger"
-	// "github.com/dgraph-io/badger/options"
-	bpb "github.com/dgraph-io/badger/pb"
-	"runtime"
-	// "fmt"
+	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/badger/pb"
 	"context"
-	"sync"
 	"log"
+	"path/filepath"
+	"runtime"
+	"sync"
+	"bytes"
+	"fmt"
 )
 
 type DBMerger struct {
-	kvStores1    *kvstore.KVStores
-	kvStores2    *kvstore.KVStores
-	KVToMerge    sync.Map
+	kvStores1 *kvstore.KVStores
+	kvStores2 *kvstore.KVStores
+	KVToMerge sync.Map
 }
 
-
-func NewMergedb(dbPath_1 string, dbPath_2 string) {
+func NewMergedb(dbsPath string, outPath string) {
 
 	// For SSD throughput (as done in badger/graphdb) see :
 	// https://groups.google.com/forum/#!topic/golang-nuts/jPb_h3TvlKE/discussion
 	runtime.GOMAXPROCS(128)
+
+	pattern := dbsPath + "/*"
+	allDBs, err := filepath.Glob(pattern)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	copy.Dir(allDBs[0], outPath)
+	allDBs = allDBs[1:]
 
 	nbOfThreads := runtime.NumCPU()
 
@@ -31,40 +41,51 @@ func NewMergedb(dbPath_1 string, dbPath_2 string) {
 		nbOfThreads = 1
 	}
 
-	kvStores1 := kvstore.KVStoresNew(dbPath_1, nbOfThreads)
+	kvStores1 := kvstore.KVStoresNew(outPath, nbOfThreads)
 
-	if (dbPath_2 != "") {
-		kvStores2 := kvstore.KVStoresNew(dbPath_2, nbOfThreads)
+	for _, db := range allDBs {
 
-		MergeStores(kvStores1.K_batch.KVStore, kvStores2.K_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.KK_batch.KVStore, kvStores2.KK_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.G_batch.KVStore, kvStores2.G_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.GG_batch.KVStore, kvStores2.GG_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.F_batch.KVStore, kvStores2.F_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.FF_batch.KVStore, kvStores2.FF_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.P_batch.KVStore, kvStores2.P_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.PP_batch.KVStore, kvStores2.PP_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.O_batch.KVStore, kvStores2.O_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.OO_batch.KVStore, kvStores2.OO_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.N_batch.KVStore, kvStores2.N_batch.KVStore, nbOfThreads)
-		MergeStores(kvStores1.NN_batch.KVStore, kvStores2.NN_batch.KVStore, nbOfThreads)
+		if db != "" {
 
-		kvStores2.Close()
+			fmt.Printf("# Merging database %s into %s...\n", db, outPath)
 
-		kvStores1.Close()
+			kvStores2 := kvstore.KVStoresNew(db, nbOfThreads)
 
-		kvStores1 = kvstore.KVStoresNew(dbPath_1, nbOfThreads)
+			wg := new(sync.WaitGroup)
+			wg.Add(12)
+
+			go MergeStores(kvStores1.K_batch.KVStore, kvStores2.K_batch.KVStore, nbOfThreads, wg)
+			go MergeStores(kvStores1.KK_batch.KVStore, kvStores2.KK_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.G_batch.KVStore, kvStores2.G_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.GG_batch.KVStore, kvStores2.GG_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.F_batch.KVStore, kvStores2.F_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.FF_batch.KVStore, kvStores2.FF_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.P_batch.KVStore, kvStores2.P_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.PP_batch.KVStore, kvStores2.PP_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.O_batch.KVStore, kvStores2.O_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.OO_batch.KVStore, kvStores2.OO_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.N_batch.KVStore, kvStores2.N_batch.KVStore, 2, wg)
+			go MergeStores(kvStores1.NN_batch.KVStore, kvStores2.NN_batch.KVStore, 2, wg)
+
+			wg.Wait()
+
+			kvStores2.Close()
+
+		}
 
 	}
 
+	kvStores1.Flush()
+
 	kvStores1.MergeKmerValues(nbOfThreads)
+
 	kvStores1.Close()
 
 }
 
+func MergeStores(kvStore1 *kvstore.KVStore, kvStore2 *kvstore.KVStore, nbOfThreads int, wg *sync.WaitGroup) {
 
-func MergeStores (kvStore1 *kvstore.KVStore, kvStore2 *kvstore.KVStore, nbOfThreads int) {
-
+	defer wg.Done()
 	// Stream keys
 	stream := kvStore2.DB.NewStream()
 
@@ -81,19 +102,40 @@ func MergeStores (kvStore1 *kvstore.KVStore, kvStore2 *kvstore.KVStore, nbOfThre
 	// KeyToList is called concurrently for chosen keys. This can be used to convert
 	// Badger data into custom key-values. If nil, uses stream.ToList, a default
 	// implementation, which picks all valid key-values.
-	stream.KeyToList = nil
+
+	// stream.KeyToList = nil
+	stream.KeyToList = func(key []byte, it *badger.Iterator) (*pb.KVList, error) {
+
+		for ; it.Valid(); it.Next() {
+
+			item := it.Item()
+			if item.IsDeletedOrExpired() {
+				break
+			}
+			if item.DiscardEarlierVersions() {
+				break
+			}
+			if ! bytes.Equal(key, item.Key()) {
+				break
+			}
+			val, err := item.ValueCopy(nil)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			key := item.KeyCopy(nil)
+
+			kvStore1.AddValueWithLock(key, val)
+		}
+
+		return nil, nil
+
+	}
 
 	// -- End of optional settings.
 
-
 	// Send is called serially, while Stream.Orchestrate is running.
-	stream.Send = func(list *bpb.KVList) error {
-		for _, kv := range list.GetKv() {
-			kvStore1.AddValue(kv.GetKey(), kv.GetValue(), 0)
-		}
-
-		return nil
-	}
+	stream.Send = nil
 
 	// // Run the stream
 	// Run the stream
